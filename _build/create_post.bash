839ce15b9f21a14dbe8ead3111fa77101109bd10
Philippe Wang <philippe.wang@gmail.com>
2013-09-24 02:44:57 +0100

    -

--- _build/create_post.bash
+++ _build/create_post.bash
@@ -23,8 +23,6 @@ include Meta
 >}
 
 {< module Post(Printer:Printer) = struct open Printer >}
-## {< let _ = !!title >}
-
 
 Lorem  ipsum dolor  sit  amet, consectetur  adipisicing  elit, sed  do
 eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
