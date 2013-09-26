4fb0bd85c6db54587a55265d376053f5b06fe6da
Philippe Wang <philippe.wang@gmail.com>
2013-09-26 23:26:59 +0100

    fix

--- _build/htmlescape.ml
+++ _build/htmlescape.ml
@@ -5,6 +5,9 @@ try
     | '&' | '<' | '>' | '\'' | '"' as c ->
       Printf.printf "&#%d;" (int_of_char c);
       esc ()
+    | '\n' ->
+      Printf.printf "\\n";
+      esc ()
     | c -> 
       print_char c; esc ()
   in esc ()
