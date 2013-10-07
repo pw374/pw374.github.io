8f16831865d220193c3c13665bbd2d8847c46ca1
Philippe Wang <philippe.wang@gmail.com>
2013-10-07 23:26:01 +0100

    fix

--- _build/tag.ml
+++ _build/tag.ml
@@ -2,6 +2,6 @@ let _ =
   List.iter
     (fun tag ->
        command ("mkdir -p '../tags/" ^ tag ^ "/'");
-       command ("ln -fs \"../../posts/" ^ id ^ "\" '../tags/" ^ tag ^ "/'");
+       command ("ln -fs \"../../" ^ id ^ "\" '../tags/" ^ tag ^ "/'");
     )
     tags
