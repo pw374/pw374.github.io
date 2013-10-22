2a7e247ef7a68c19c49722f2d37c3a0228743e30
Philippe Wang <philippe.wang@gmail.com>
2013-10-22 01:29:18 +0100

    fix

--- sync/gen.bash
+++ sync/gen.bash
@@ -14,10 +14,10 @@ done
 if [[ "$#" == "0" ]]
 then
     echo "Usage:"
-    echo "$0 src/md/file.md"
-    echo "$0 src/md/file.html"
-    echo "$0 src/md/"
-    echo "$0 src/md/some-directory"
+    echo "$0 site/file.md"
+    echo "$0 site/file.html"
+    echo "$0 site/"
+    echo "$0 site/some-directory"
     exit 1
 fi
 
@@ -25,7 +25,7 @@ IFS=
 find "$@" -type f -iname '*.md' |
 while read i
 do
-    target="$(sed -e 's/\.md$/.html/' -e 's/^md-pages/html-pages/' <<< "$i")"
+    target="$(sed -e 's/\.md$/.html/' -e 's/^site/ocaml.org/' <<< "$i")"
     mkdir -p "$(dirname "$target")"
     make -f Makefile.from_md "$target"
 done
@@ -33,7 +33,7 @@ done
 find "$@" -type f -iname '*.html' |
 while read i
 do
-    target="$(sed -e 's/^md-pages/html-pages/' <<< "$i")"
+    target="$(sed -e 's/^site/ocaml.org/' <<< "$i")"
     mkdir -p "$(dirname "$target")"
     make -f Makefile.from_html "$target"
 #    mpp -so '((!' -sc '!))' -son '{{!' -scn '!}}' -soc '' -scc '' -sec '' -its < "$i" > "$target"
