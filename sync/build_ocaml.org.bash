158bdec973267d8dd1ab1ab1870b228b2e3e62e2
Philippe Wang <philippe.wang@gmail.com>
2013-11-14 00:49:07 +0000

    Update build_ocaml.org.bash

--- sync/build_ocaml.org.bash
+++ sync/build_ocaml.org.bash
@@ -18,10 +18,22 @@ FLAG=$HOME/flag/force-build.flag
 
 while true
 do
+
+# begin opam package list matter
+### .package-list-old is always up to date if it does exist, hence this trick:
+cp ~/opam-repository/packages/.package-list-old opam-update-list || cp ~/opam-repository/packages/.package-list opam-update-list
+if cp ~/opam-repository/packages/.package-list-old opam-update-list-old
+then
+    mv opam-update-list-old opam-update-list
+fi
+sort -r opam-update-list | head -n 6| cut -f2 > opam-update-list.tmp
+mv opam-update-list.tmp opam-update-list
+# end opam package list matter
+
     if (( i % 20 == 0 ))
     then
-i=1
-rm -fr ~/ocaml.org/ocaml.org/
+	i=1
+	rm -fr ~/ocaml.org/ocaml.org/
     fi
     cd ~/ocaml.org/ || exit 1
     git pull
@@ -30,11 +42,11 @@ rm -fr ~/ocaml.org/ocaml.org/
     (( i ++ ))
     for s in stdout stderr
     do
-klm="$(du -s $HOME/$s.log|sed 's|\t.*||g')"
-if (( klm > 3000 ))
-then
-    gzip -c $HOME/$s.log > $HOME/$s.log.gz
-fi
+	klm="$(du -s $HOME/$s.log|sed 's|\t.*||g')"
+	if (( klm > 3000 ))
+	then
+	    gzip -c $HOME/$s.log > $HOME/$s.log.gz
+	fi
     done
     date
     echo "Next build in 200 seconds, or maybe less."
@@ -42,13 +54,13 @@ fi
     echo "touch ${FLAG}"
     for ((x=0;x<100;x++))
     do
-sleep 2
-if [[ -f "${FLAG}" ]]
-then
-    rm -f "${FLAG}"
-    i=20
-    break
-fi
+	sleep 2
+	if [[ -f "${FLAG}" ]]
+	then
+	    rm -f "${FLAG}"
+	    i=20
+	    break
+	fi
     done
     
 done > $HOME/stdout.log 2> $HOME/stderr.log
