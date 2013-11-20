f569839cac3c048bf2bef130743fb541054e9113
Philippe Wang <philippe.wang@gmail.com>
2013-11-20 14:43:17 +0000

    Update make_opam_package_list.bash

--- sync/make_opam_package_list.bash
+++ sync/make_opam_package_list.bash
@@ -1,6 +1,21 @@
 #!/bin/bash
 
-
-while true ; do cd ~/opam-repository/ && git pull && cd packages && mv .package-list{,-old} ; for i in * ; do v=$(echo "$i"|sed 's/[^.][^.]*\.//') ; p=$(echo "$i"|sed 's/\..*//g') ; d="$(git log -n 1 $i|grep 'Date:'|head -n 1|sed 's/[^:]*: *//'|sed 's/\(201[234]\).*/\1/')" ; echo -e "$(date -d "$d" +%Y%m%d%H%M%S)\t<tr><td><a href='http://opam.ocaml.org/pkg/$p/$v/'>$p</a></td><td><a href='http://opam.ocaml.org/pkg/$p/$v/'>$v</a></td><td>$(date -d "$d" "+%d %b %Y")</td></tr>" ; done > .package-list ; rm -f .package-list-old; sleep 3600; done
-
+while true ;
+do cd ~/opam-repository/ && git pull && cd packages && mv .package-list{,-old} ;
+    for i in $(ls -1dt *) ;
+    do v=$(echo "$i"|sed 's/[^.][^.]*\.//') ;
+	p=$(echo "$i"|sed 's/\..*//g') ;
+	d="$(git log -n 1 $i|grep 'Date:'|head -n 1|sed 's/[^:]*: *//'|sed 's/\(201[234]\).*/\1/')" ;
+	if grep -q "$p" .package-list
+	then
+	    true
+	else
+	    echo -e "$(date -d "$d" +%Y%m%d%H%M%S)\t<tr><td><a href='http://opam.ocaml.org/pkg/$p/$v/'>$p</a></td><td><a href='http://opam.ocaml.org/pkg/$p/$v/'>$v</a></td><td>$(date -d "$d" "+%d %b %Y")</td></tr>" ;
+	fi
+    done > .package-list ;
+    rm -f .package-list-old;
+    echo "Sleeping now for 3600 seconds..."
+    sleep 3600;
+    echo "Done sleeping."
+done
 
