488fec4916368de0546271756be1d2f4a0bc7064
Philippe Wang <philippe.wang@gmail.com>
2013-11-21 13:57:02 +0000

    Update make_opam_package_list.bash

--- sync/make_opam_package_list.bash
+++ sync/make_opam_package_list.bash
@@ -1,21 +1,25 @@
 #!/bin/bash
 
 while true ;
-do cd ~/opam-repository/ && git pull && cd packages && mv .package-list{,-old} ;
-    for i in $(ls -1dt *) ;
-    do v=$(echo "$i"|sed 's/[^.][^.]*\.//') ;
+do 
+    cd ~/opam-repository/packages && git pull
+    for j in $(ls -1dt *)
+    do
+	i="$(cd "$j" && ls -1dt "$j"*|head -n 1)";
+	v=$(echo "$i"|sed 's/[^.][^.]*\.//') ;
 	p=$(echo "$i"|sed 's/\..*//g') ;
-	d="$(git log -n 1 $i|grep 'Date:'|head -n 1|sed 's/[^:]*: *//'|sed 's/\(201[234]\).*/\1/')" ;
+	d="$(git log -n 1 "$j/$i"|grep 'Date:'|head -n 1|sed 's/[^:]*: *//'|sed 's/\(201[234]\).*/\1/')" ;
 	if grep -q "$p" .package-list
 	then
 	    true
 	else
 	    echo -e "$(date -d "$d" +%Y%m%d%H%M%S)\t<tr><td><a href='http://opam.ocaml.org/pkg/$p/$v/'>$p</a></td><td><a href='http://opam.ocaml.org/pkg/$p/$v/'>$v</a></td><td>$(date -d "$d" "+%d %b %Y")</td></tr>" ;
 	fi
-    done > .package-list ;
-    rm -f .package-list-old;
+    done > .package-list-tmp;
+    cp .package-list{,-old}; 
+    cp .package-list{-tmp,};
+    rm .package-list-{old,tmp}
     echo "Sleeping now for 3600 seconds..."
     sleep 3600;
     echo "Done sleeping."
 done
-
