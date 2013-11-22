5125a7782f03259ba7c1354836b4b7334ef9f030
Philippe Wang <philippe.wang@gmail.com>
2013-11-22 02:12:23 +0000

    Update make_opam_package_list.bash

--- sync/make_opam_package_list.bash
+++ sync/make_opam_package_list.bash
@@ -1,20 +1,19 @@
 #!/bin/bash
-
+	
 while true ;
 do 
     cd ~/opam-repository/packages && git pull
     for j in $(ls -1dt *)
     do
+        if echo $j | grep -q '\.'
+	then
+	    continue
+	fi
 	i="$(cd "$j" && ls -1dt "$j"*|head -n 1)";
 	v=$(echo "$i"|sed 's/[^.][^.]*\.//') ;
 	p=$(echo "$i"|sed 's/\..*//g') ;
 	d="$(git log -n 1 "$j/$i"|grep 'Date:'|head -n 1|sed 's/[^:]*: *//'|sed 's/\(201[234]\).*/\1/')" ;
-	if grep -q "$p" .package-list
-	then
-	    true
-	else
-	    echo -e "$(date -d "$d" +%Y%m%d%H%M%S)\t<tr><td><a href='http://opam.ocaml.org/pkg/$p/$v/'>$p</a></td><td><a href='http://opam.ocaml.org/pkg/$p/$v/'>$v</a></td><td>$(date -d "$d" "+%d %b %Y")</td></tr>" ;
-	fi
+	echo -e "$(date -d "$d" +%Y%m%d%H%M%S)\t<tr><td><a href='http://opam.ocaml.org/pkg/$p/$v/'>$p</a></td><td><a href='http://opam.ocaml.org/pkg/$p/$v/'>$v</a></td><td>$(date -d "$d" "+%d %b %Y")</td></tr>" ;
     done > .package-list-tmp;
     cp .package-list{,-old}; 
     cp .package-list{-tmp,};
@@ -25,3 +24,4 @@ do
     date
     echo "Done sleeping."
 done
+
