51ae35a86fdf7a6c1d2ce63221f5c66a0adde7ed
Philippe Wang <philippe.wang@gmail.com>
2013-12-26 01:47:15 +0100

    Update make_opam_package_list.bash

--- sync/make_opam_package_list.bash
+++ sync/make_opam_package_list.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-	
+
 while true ;
 do 
     cd ~/opam-repository/packages && git pull
@@ -13,7 +13,7 @@ do
 	v=$(echo "$i"|sed 's/[^.][^.]*\.//') ;
 	p=$(echo "$i"|sed 's/\..*//g') ;
 	d="$(git log -n 1 "$j/$i"|grep 'Date:'|head -n 1|sed 's/[^:]*: *//'|sed 's/\(201[234]\).*/\1/')" ;
-	echo -e "$(date -d "$d" +%Y%m%d%H%M%S)\t<tr><td><a href='http://opam.ocaml.org/pkg/$p/$v/'>$p</a></td><td><a href='http://opam.ocaml.org/pkg/$p/$v/'>$v</a></td><td>$(date -d "$d" "+%d %b %Y")</td></tr>" ;
+	echo -e "$(date -d "$d" +%Y%m%d%H%M%S)\t<tr><td><a href='http://opam.ocaml.org/pkg/$p/$p.$v/'>$p</a></td><td><a href='http://opam.ocaml.org/pkg/$p/$p.$v/'>$v</a></td><td>$(date -d "$d" "+%d %b %Y")</td></tr>" ;
     done > .package-list-tmp;
     cp .package-list{,-old}; 
     cp .package-list{-tmp,};
